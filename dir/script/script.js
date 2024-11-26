5a25db747ff6fb1e5a6e5d0c7c0c1b3b3fc909d0
Watterbenders <kristjan.gersak@student.um.si>
2024-11-26 17:53:21 +0100

    GUMBI!!!!!!!!!!!
    upam da delajo sam nej en dark mode naštima k je več cssa kokr programiranja

--- dir/script/script.js
+++ dir/script/script.js
@@ -4,4 +4,24 @@
         -Gumbi
         -...
         -ne vem kaj še
+
+    Za gumbe uporabli addEventListener('click', [ime gumba v htmlju])
 */
+
+//button.addEventListener('click', function(event) {
+//    console.log('Button clicked!');
+//}, { passive: true });
+
+
+window.onload = (event) => {
+    //testiru bom mau a je fajn on load uporablat al kaj bi blo najbolš pr web developmentu
+    console.log("page is fully loaded");
+    
+    const vrni = document.getElementById('Return');
+    const crni = document.getElementById('DarkMode');
+
+    vrni.addEventListener('click', function(event){
+        window.location.href = "../public/index.html"
+    });
+};
+
