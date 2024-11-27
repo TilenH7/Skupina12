1d10be2c422d0e4b567dd6e022bed9903bbf2ea1
Watterbenders <kristjan.gersak@student.um.si>
2024-11-27 10:47:39 +0100

    KOKI KLIKR DELA

--- dir/script/piskot.js
+++ dir/script/piskot.js
@@ -1,9 +1,11 @@
 window.onload = (event)=> {
     const piskot = document.getElementById('piskot');
+    const counterElement = document.getElementById('counter');
 
     let pritiski = 0;
 
     piskot.addEventListener('click',(event)=>{
         pritiski++;
+        counterElement.textContent = pritiski;
     });
 };
