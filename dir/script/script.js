4a1cfd7711b9ffe24d34b327288db0830222a0e3
Watterbenders <kristjan.gersak@student.um.si>
2024-11-27 11:27:35 +0100

    DELA

--- dir/script/script.js
+++ dir/script/script.js
@@ -16,30 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const vrni = document.getElementById('Return');
     const crni = document.getElementById('DarkMode');
 
-    vrni.addEventListener('click', function(event){
-        window.location.href = "../public/index.html"
-    });
-    crni.addEventListener('click', function(event){
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-    });
+    if(vrni){
+        vrni.addEventListener('click', function(event){
+            window.location.href = "../public/index.html"
+        });
+    };
+    if (crni) {
+        crni.addEventListener('click', function(event){
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+        });
+    }
+    const navigateTo = (id, target) => {
+        const button = document.getElementById(id);
+        if (button) {
+            button.addEventListener('click', () => {
+                window.location.href = target;
+            });
+        }
+    };
 
-    const cookie = document.getElementById('piskotLINK');
-    const puska = document.getElementById('puskaLINK');
-    const slike = document.getElementById('slikeLINK');
-    const bombomet = document.getElementById('bombometLINNK');
-
-    cookie.addEventListener('click', function(event){
-        window.location.href = "../public/CookieClicker.html"
-    });
-    puska.addEventListener('click', function(event){
-        window.location.href = "../public/puska.html"
-    });
-    slike.addEventListener('click', function(event){
-        window.location.href = "../public/slike.html"
-    });
-    bombomet.addEventListener('click', function(event){
-        window.location.href = "../public/bombomet.html"
-    });
+    navigateTo('piskotLINK', '../public/CookieClicker.html');
+    navigateTo('puskaLINK', '../public/puska.html');
+    navigateTo('slikeLINK', '../public/slike.html');
+    navigateTo('bombometLINNK', '../public/bombomet.html');
 });
 
